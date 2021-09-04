2e880f2b910a6d2a853c32ee500772bb2ebdc4ee
Leonardo <leonardomkwii@gmail.com>
2021-09-04 22:16:51 +0200

    opdracht 3

---
+++ H02/Opdracht 2.php
@@ -0,0 +1,45 @@
+<?php
+
+//1ste zin
+
+$a1 = "Ik heb niet gefaald.";
+$b1 = "die niet werken.";
+$c1 = "1000";
+
+$volledigeZin1 = $a1 . " Ik heb alleen " . $c1 . " manieren gevonden " . $b1;
+
+echo $volledigeZin1;
+echo "<br>";
+
+//2de zin
+
+$a2 = "die geen fout";
+$b2 = "en";
+$c2 = "niet.";
+
+$volledigeZin2 = "Mensen " . $a2 . $b2 . " maken, " . " werken " . $c2;
+
+echo $volledigeZin2;
+echo "<br>";
+
+//3de zin
+
+$a3 = "ry";
+$b3 = ", but";
+$c3 = "fail";
+
+$volledigeZin3 = "T" . $a3 . " and " . $c3 . $b3 . " never " . $c3 . " to t" . $a3 . ".";
+
+echo $volledigeZin3;
+echo "<br>";
+
+//4de zin
+
+$a4 = "\"omgaan";
+$b4 = "met";
+$c4 = "teleu";
+
+$volledigeZin4 = "De cursus " . $a4 . " " . $b4 . " " . $c4 . "rstellingen\"" . " kan vanavond helaas niet doorgaan.";
+
+echo $volledigeZin4;
+echo "<br>";
